720bdfbf82bc054afe5a29568d54a9761ce25ac7
Davi Patricio <davipatricio2006@gmail.com>
2023-02-04 02:22:25 +0000

    improve picker ux

--- src/components/Dashboard/RangeInfo/index.tsx
+++ src/components/Dashboard/RangeInfo/index.tsx
@@ -1,46 +1,47 @@
 "use client";
 
-import { BsCalendar2Event } from "react-icons/bs";
+import { DateRangePicker, Range } from "react-date-range";
 import { AiOutlineReload } from "react-icons/ai";
-import { Calendar, DateRangePicker } from "react-date-range";
+import { BsCalendar2Event } from "react-icons/bs";
 
-import styles from "./index.module.scss";
 import { useCallback, useEffect, useRef, useState } from "react";
+import styles from "./index.module.scss";
 
-import "react-date-range/dist/styles.css"; // main style file
-import "react-date-range/dist/theme/default.css"; // theme css file
+import "react-date-range/dist/styles.css";
+import "react-date-range/dist/theme/default.css";
 
 export default function RangeInfo() {
   const [menuOpen, setMenuOpen] = useState(false);
 
-  const selectionRange = {
-    startDate: new Date(),
-    endDate: new Date(),
-    key: "selection",
-  };
+  const currentDate = new Date();
+
+  const [state, setState] = useState<Range[]>([
+    {
+      startDate: new Date(currentDate.getFullYear(), currentDate.getMonth(), 1),
+      endDate: currentDate,
+      key: "selection",
+    },
+  ]);
 
   const pickerDivRef = useRef<HTMLDivElement>(null);
 
-  const handleClick = (e: React.MouseEvent<HTMLDivElement>) => {
+  const handleClick = useCallback((e: React.MouseEvent<HTMLDivElement>) => {
     e.stopPropagation();
     setMenuOpen(true);
 
-    {
-      /* move  ref element to mouse position */
-    }
+    // move date picker to mouse position
     if (pickerDivRef.current) {
       pickerDivRef.current.style.top = `${e.clientY}px`;
       pickerDivRef.current.style.left = `${e.clientX}px`;
     }
-  };
+  }, []);
 
   const handleOutsideCalendarClick = useCallback(
     (e: MouseEvent) => {
-      if (menuOpen && !pickerDivRef.current?.contains(e.target as Node)) {
+      if (menuOpen && !pickerDivRef.current?.contains(e.target as Node))
         setMenuOpen(false);
-      }
     },
-    [menuOpen]
+    [menuOpen, pickerDivRef]
   );
 
   useEffect(() => {
@@ -71,7 +72,13 @@ export default function RangeInfo() {
 
         <div ref={pickerDivRef} style={{ position: "absolute" }}>
           {menuOpen && (
-            <DateRangePicker ranges={[selectionRange]} onChange={() => {}} />
+            <DateRangePicker
+              ranges={state}
+              months={1}
+              minDate={new Date(currentDate.getFullYear(), 0, 1)}
+              maxDate={currentDate}
+              onChange={(item) => setState([item.selection])}
+            />
           )}
         </div>
       </div>
